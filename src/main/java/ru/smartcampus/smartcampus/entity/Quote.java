318e7af0f3dab0bd9e879bd5b8c8f17b89e35e5a
glebpad <glebpad@yandex.ru>
2023-06-06 22:15:30 +0300

    Models, dto

---
+++ src/main/java/ru/smartcampus/smartcampus/entity/Quote.java
@@ -0,0 +1,14 @@
+package ru.smartcampus.smartcampus.entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+public class Quote {
+
+    private String description;
+
+}
